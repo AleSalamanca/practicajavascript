2b42e56d31d2e2d548541aa3321796eab7305f78
jesicavictora <jesicavictora@gmail.com>
2021-05-11 15:35:51 +0200

    Ejercicio 13 arrays

---
+++ Modulo01/Ejercicio13.js
@@ -0,0 +1,8 @@
+//Consigue el valor "Volvo" del array de cars y muestralo por consola.
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+//RESPUESTA//
+
+console.log(cars[1]); // para acceder a una posicion de un array se utiliza [x], La primer posicion es 0
