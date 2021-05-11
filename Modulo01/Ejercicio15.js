6e20cf7064bd284d445b291694f2e731667b92f9
jesicavictora <jesicavictora@gmail.com>
2021-05-11 15:40:47 +0200

    Ejercicio 15 arrays

---
+++ Modulo01/Ejercicio15.js
@@ -0,0 +1,9 @@
+//Alerta el numero de elementos en el array usando la propiedad correcta de Array.
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+
+//RESPUESTA//
+
+alert(cars.length); // El .length te devuelve la cantidad de propiedades que contiene un array. 
