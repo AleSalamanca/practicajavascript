67158f68fc061a2c18178eaa54da417051c5844f
jesicavictora <jesicavictora@gmail.com>
2021-05-11 15:10:49 +0200

    Ejercicio 06 suma

---
+++ Modulo01/Ejercicio06.js
@@ -0,0 +1,9 @@
+//Dado el siguiente javascript, imprime con un `console.log` la suma del precio de ambos juguetes.
+
+
+const toy1 = { name: "Buss myYear", price: 19 };
+const toy2 = { name: "Rallo mcKing", price: 29 };
+
+//RESULTADO//
+console.log(toy1.price + toy2.price);
+//Al poner 2 variables numericas con un + las suma, si pusieras toy1.name + toy2.price el resultado seria Buss myYear29 
