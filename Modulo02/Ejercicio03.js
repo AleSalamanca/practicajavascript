80b30dcf884092ce948feef2ebf8365a8365d83d
jesicavictora <jesicavictora@gmail.com>
2021-05-12 11:19:54 +0200

    Ejercicio 03 forof

---
+++ Modulo02/Ejercicio03.js
@@ -0,0 +1,14 @@
+/*Usa un bucle forof para recorrer todos los destinos del array.
+ Imprime en un console log sus valores.
+*/
+
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+
+//RESULTADO//
+
+for (const city of placesToTravel) {
+
+    console.log(city);
+    
+}
