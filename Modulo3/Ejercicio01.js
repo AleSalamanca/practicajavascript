e4d2e1ab15aed411b7b966bd83c64a2b26c46623
jesicavictora <jesicavictora@gmail.com>
2021-05-13 10:21:53 +0200

    Ejercicio 01 querySelector

---
+++ Modulo3/Ejercicio01.js
@@ -0,0 +1,11 @@
+/*Español
+Dado el siguiente html usa querySelector para mostrar por consola el botón con la clase .showme
+*/
+
+//RESULTADO//
+function showme(){ //se debe incluir el onclic llamando al showme en el html
+
+    let DOMelement = document.querySelector(".showme");
+    console.log(DOMelement);
+
+}
