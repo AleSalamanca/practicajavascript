cde9365eddfa2aa6663d502e6d7f238a123d593a
jesicavictora <jesicavictora@gmail.com>
2021-05-13 10:51:59 +0200

    Ejercicio 05 querySelector/InnerHTML

---
+++ Modulo3/Ejercicio05.js
@@ -0,0 +1,7 @@
+//Basandote en el siguiente html, inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
+
+
+
+let DOMElement = document.querySelector(".fn-insert-here");
+
+DOMElement.innerHTML="Wubba Lubba dub dub";
