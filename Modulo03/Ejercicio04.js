3c8536a8711787dc560b1653b794a39385999dff
jesicavictora <jesicavictora@gmail.com>
2021-05-13 11:30:34 +0200

    Ejercicio 04 innerHTML

--- Modulo03/Ejercicio04.js
+++ Modulo03/Ejercicio04.js
@@ -1,6 +1,7 @@
 //Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
 
 let NewP = document.createElement("p")
+
 NewP.innerHTML = "Soy dinámico!";
 
 document.body.appendChild(NewP);
