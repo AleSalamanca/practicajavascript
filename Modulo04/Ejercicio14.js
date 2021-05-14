ea0facabebccbe4559c37cb0365f071c845561e9
jesicavictora <jesicavictora@gmail.com>
2021-05-14 15:50:48 +0200

    Ejercicio 14 filter

---
+++ Modulo04/Ejercicio14.js
@@ -0,0 +1,18 @@
+/*Dado el siguiente array, utiliza .filter() 
+para generar un nuevo array con los valores que sean mayor que 18
+*/
+
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+//RESULTADO//
+
+
+const list = ages.filter((age)=>{
+
+    if(age>=18){
+        return age;
+    }
+
+});
+
+console.log(list);
