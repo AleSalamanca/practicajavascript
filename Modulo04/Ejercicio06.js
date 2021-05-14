83cb44a3a4344af177611f83974215660b7c1ebf
jesicavictora <jesicavictora@gmail.com>
2021-05-14 12:16:44 +0200

    Ejercicio 06 spread operators

---
+++ Modulo04/Ejercicio06.js
@@ -0,0 +1,11 @@
+//Dado el siguiente array, crea una copia usando spread operators.
+
+
+const pointsList = [32, 54, 21, 64, 75, 43]
+
+//RESULTADO//
+
+
+const pointsList2 = [...pointsList];
+
+console.log(pointsList2);
