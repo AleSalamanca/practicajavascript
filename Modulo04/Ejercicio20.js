daf17d5be9d863c161f580f38941c64e2f22e533
jesicavictora <jesicavictora@gmail.com>
2021-05-18 14:20:35 +0200

    Ejercicio 20 .find

---
+++ Modulo04/Ejercicio20.js
@@ -0,0 +1,11 @@
+//Dado el siguiente array, usa .find() para econtrar el número 100.
+
+
+const numbers = [32, 21, 63, 95, 100, 67, 43];
+
+
+//RESULTADO//
+
+const found = numbers.find(number=> number == 100);
+
+ console.log(found);
