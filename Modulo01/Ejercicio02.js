6046beccfb1146d17fea6babe1992ee5c6dcfea4
unknown <alesala_@hotmail.com>
2022-04-22 20:29:09 -0300

    feat: ejercicio 2

--- Modulo01/Ejercicio02.js
+++ Modulo01/Ejercicio02.js
@@ -2,6 +2,7 @@
 
 // Al igual que el ejercicio anterior se puede usar var, let o const. 
 
-const x = 50 ; // Como es numero y no es string el valor no va entre comillas, si lo pusieras entre comillas pasa a ser un string.
+//const x = 50 ; // Como es numero y no es string el valor no va entre comillas, si lo pusieras entre comillas pasa a ser un string.
 
+let x = 50;git 
 console.log(x);
