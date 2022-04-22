144c9946c772d98026db0a2fa9246c81af3220fb
unknown <alesala_@hotmail.com>
2022-04-22 20:25:03 -0300

    feat: new var carName

--- Modulo01/Ejercicio01.js
+++ Modulo01/Ejercicio01.js
@@ -2,9 +2,9 @@
 
 
 //Este ejercicio puede tener multiples respuestas
-var carName = "Volvo";  //Var le da un ambito global y su valor puede ser modificado.
+//var carName = "Volvo";  //Var le da un ambito global y su valor puede ser modificado.
 
 // let carName = "Volvo"; let le da un ambito local y su valor tambien puede ser modificado.
 // const  carName = "Volvo"; es de ambito local al igual que let y su valor es constante no se puede modificar.
 
-console.log(carName);
+var carName= "volvo";
